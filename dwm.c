4dafb9a9e2fe0f8d9a8303f75cf2695eb3b809bd
ImFlush <tompourie@gmail.com>
2020-12-21 21:23:46 +0100

    semicolon missing fixed

--- dwm.c
+++ dwm.c
@@ -726,7 +726,7 @@ drawbar(Monitor *m)
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
-	drw_setscheme(drw, scheme[SchemeTagsNorm])
+	drw_setscheme(drw, scheme[SchemeTagsNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - x) > bh) {
