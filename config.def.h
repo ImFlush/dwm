075445552a375f1632613dc3c58eff12fadd01b4
ImFlush <tompourie@gmail.com>
2021-01-03 07:57:16 +0100

    telegram icon changed

--- config.def.h
+++ config.def.h
@@ -28,17 +28,20 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "󰆍", "󰊯", "3", "4", "5", "6", "7", "󰓓", "󰙯" };
+static const char *tags[] = { "󰆍", "󰊯", "3", "4", "5", "6", "󰔁", "󰓓", "󰙯" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "discord",  	NULL,       NULL,       1 << 8,   	0,           -1 },
-	{ "Steam",  	NULL,       NULL,       1 << 7,   	0,           -1 },
-	{ "Brave-browser",  	NULL,       NULL,       1 << 1,   	0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "discord",  			NULL,     NULL,       	  1 << 8,    0,          0,			  0,	    -1 },
+	{ "Steam",  			NULL,     NULL,       	  1 << 7,    0,          0, 		  0,   		-1 },
+	{ "Brave-browser",  	NULL,     NULL,       	  1 << 1,  	 0,          0, 		  0, 		-1 },
+	{ "St",      			NULL,     NULL,           0,         0,          0,           0,        -1 },
+	{ "TelegramDesktop",    NULL,     NULL,           1 << 6,    0,          0,           0,        -1 },
+	{ NULL,      			NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
