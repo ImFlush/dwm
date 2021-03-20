ad250d8edbc5689e604e12245cfdb80e86ace063
ImFlush <tompourie@gmail.com>
2021-03-20 15:12:39 +0100

    xresources variables outsourced

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static unsigned int borderpx  		= 3;        /* border pixel of windows */
+static unsigned int gappx     		= 10;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -76,6 +76,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "normbgcolor",        STRING,  "" },
+		{ "borderpx",          	INTEGER, &borderpx },
+		{ "gappx",          	INTEGER, &gappx },
+};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
